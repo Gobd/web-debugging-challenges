675c6a8bf903cb2bf76f65204f7c0e9c1bdccd82
Brian Kemper <bkemper@gmail.com>
2016-06-09 01:49:40 -0600

    working

--- challenge4/code.js
+++ challenge4/code.js
@@ -1,30 +1,24 @@
-
 // given the following array, randomize it.
 
 var students = [
-  'Dan'
-, 'Brack'
-, 'Erin'
-, 'Geordyn'
-, 'Jess'
-, 'Sarah'
-, 'Andrew'
-, 'Ryan'
-, 'Tim'
-, 'Stephen'
-, 'David'
-, 'Mark'
+    'Dan', 'Brack', 'Erin', 'Geordyn', 'Jess', 'Sarah', 'Andrew', 'Ryan', 'Tim', 'Stephen', 'David', 'Mark'
 ]
 
-function randomArray (arr) {
-  function randomNumber () {
-    return Math.floor(Math.random() * arr.length)
-  }
-  var newArr = []
-  while(arr.length >= 0){
-    newArr.push(arr.slice(randomNumber(), 1)[0])
-  }
-  return newArr
+function randomArray(arr) {
+  // random function that takes in max # arg
+  // makea  copy of the array so we don't modify it
+    let newArr = [],
+        rand = (max) => Math.floor(Math.random() * max),
+        copy = [].concat(arr);
+
+// while our shuffled array is shorter than the original, keep shuffling
+// we just splice out one number at a time until the lengths match
+    while (newArr.length < arr.length) {
+        newArr.push(copy.splice(rand(copy.length), 1)[0]);
+    }
+    return newArr;
 }
 
 console.log(randomArray(students));
+// doesn't modify the original array!
+// console.log(students);
